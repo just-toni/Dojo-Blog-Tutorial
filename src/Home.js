7454717b81084a818149db811fc0b15d78f0de29
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-10-19 23:56:15 +0100

    dojo-blog-tutorial

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -24,10 +24,24 @@ const Home = () => {
          id: 4
         }
     ]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id)
+        setBlogs(newBlogs);
+    }
+
+    const[name, setName] = useState('luigi')
+
+    useEffect(()=> {
+        console.log('use effect ran')
+        console.log(name)
+    }, [name]);
+
     return ( 
         <div className="home">
-            <BlogList blogs={ blogs } title="All Blogs!"/>
-            <BlogList blogs={ blogs.filter ((blog) => blog.author === 'yoshi') } title="Yoshi's Blogs!"/>
+            <BlogList blogs={ blogs } title="All Blogs!" handleDelete={handleDelete}/>
+            <button onClick={() => setName('mario')}>Change Name</button>
+            <p>{ name }</p>
         </div>
     );
 }
