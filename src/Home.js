df238bf9b05c19e654b50bab1296bce64cc985bb
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-10-19 21:50:00 +0100

    dojo-blog-tutorial

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,31 @@
+import { useState } from "react";
+
 const Home = () => {
-    const handleClick = () => {
-        console.log("Hello, ninjas");
-    }
-
-    const handleClickAgain = (name) => {
-        console.log('hello, ' + name)
-    }
-
+    const [blogs, setBlogs] = useState([
+        {title: 'My new website',
+         body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum nesciunt officia fugiat cumque animi, dignissimos porro culpa, hic mollitia quos velit explicabo dolorum perferendis, consectetur id sequi enim dolore aspernatur!',
+         author: 'mario',
+         id: 1,
+        },
+        {title: 'Welcome party!',
+         body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum nesciunt officia fugiat cumque animi, dignissimos porro culpa, hic mollitia quos velit explicabo dolorum perferendis, consectetur id sequi enim dolore aspernatur!',
+         author: 'lugui',
+         id: 2
+        },
+        {title: 'Unknown',
+         body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum nesciunt officia fugiat cumque animi, dignissimos porro culpa, hic mollitia quos velit explicabo dolorum perferendis, consectetur id sequi enim dolore aspernatur!',
+         author: 'yoshi',
+         id: 3
+        }
+    ]);
     return ( 
         <div className="home">
-            <h1>Homepage</h1>
-            <button onClick={handleClick}>Click Me</button>
-            <button onClick={handleClickAgain("toni")}>Click Me Again</button>
+            {blogs.map = (blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                </div>
+            )}
         </div>
     );
 }
