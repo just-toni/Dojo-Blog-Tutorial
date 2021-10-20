eb24b8abc8c740617fa4503622a6301d5b2704f6
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-10-20 22:04:01 +0100

    dojo blog tutorial

--- src/Home.js
+++ src/Home.js
@@ -2,28 +2,7 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: 'My new website',
-         body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum nesciunt officia fugiat cumque animi, dignissimos porro culpa, hic mollitia quos velit explicabo dolorum perferendis, consectetur id sequi enim dolore aspernatur!',
-         author: 'mario',
-         id: 1,
-        },
-        {title: 'Welcome party!',
-         body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum nesciunt officia fugiat cumque animi, dignissimos porro culpa, hic mollitia quos velit explicabo dolorum perferendis, consectetur id sequi enim dolore aspernatur!',
-         author: 'lugui',
-         id: 2
-        },
-        {title: 'Unknown',
-         body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum nesciunt officia fugiat cumque animi, dignissimos porro culpa, hic mollitia quos velit explicabo dolorum perferendis, consectetur id sequi enim dolore aspernatur!',
-         author: 'yoshi',
-         id: 3
-        },
-        {title: 'Random',
-         body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum nesciunt officia fugiat cumque animi, dignissimos porro culpa, hic mollitia quos velit explicabo dolorum perferendis, consectetur id sequi enim dolore aspernatur!',
-         author: 'yoshi',
-         id: 4
-        }
-    ]);
+    const [blogs, setBlogs] = useState([]);
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id)
@@ -33,15 +12,13 @@ const Home = () => {
     const[name, setName] = useState('luigi')
 
     useEffect(()=> {
-        console.log('use effect ran')
-        console.log(name)
-    }, [name]);
+        fetch("http://localhost:8000/blogs")
+        .then
+    }, []);
 
     return ( 
         <div className="home">
             <BlogList blogs={ blogs } title="All Blogs!" handleDelete={handleDelete}/>
-            <button onClick={() => setName('mario')}>Change Name</button>
-            <p>{ name }</p>
         </div>
     );
 }
